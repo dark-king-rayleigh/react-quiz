627663546145ab80a560682050dc5be15ecaf892
Ayush Baral <ayushbaral17@gmail.com>
2022-06-19 13:24:02 +0545

    feat(questions:randomise questions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Questions from "../components/Questions";
 import Result from "../components/Result";
 import { questions } from "../data/questions";
+import { Question } from "../types";
 
 const Home: NextPage = () => {
+  const [quizQuestions, setQuizQuestions] = useState<Question[] | []>([]);
   const [questionIndex, setQuestionIndex] = useState(0);
   const [showResult, setShowResult] = useState(false);
   const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    // randomize questions
+    const shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    setQuizQuestions(shuffledQuestions);
+  }, [showResult]);
 
   return (
     <>
@@ -19,7 +27,7 @@ const Home: NextPage = () => {
       <main className="flex w-full h-screen flex-col items-center justify-center text-center bg-secondary m-0 p-0">
         {!showResult ? (
           <Questions
-            question={questions[questionIndex]}
+            question={quizQuestions[questionIndex]}
             questionIndex={questionIndex}
             onSetQuestionIndex={setQuestionIndex}
             onSetShowResult={setShowResult}
